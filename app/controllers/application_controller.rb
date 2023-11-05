51847bf9e0162d8f5e3f9f1dcc57598d8df1056a
yusuke-sudou <143806854+yusuke-sudou@users.noreply.github.com>
2023-11-05 07:21:30 +0000

    [Add]管理者側ログイン・ログアウト関係実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,34 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  
+  def after_sign_up_path_for(resource)
+    top_path
+  end
+  
+  # ログイン後の画面遷移先設定
+  def after_sign_in_path_for(resource_or_scope)
+    if resource_or_scope.is_a?(Admin)
+      # adminユーザー
+      admin_menus_path
+      
+    else
+      # publicユーザー
+      top_path
+    end
+  end
+  
+  # ログアウト後の画面遷移先設定
+  def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :admin
+      # adminユーザー
+      new_admin_session_path
+      
+    else
+      # publicユーザー
+      root_path
+    end
+  end
 
   protected
 
@@ -7,11 +36,4 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
   
-  def after_sign_up_path_for(resource)
-    top_path
-  end
-  
-  def after_sign_in_path_for(resource)
-    top_path
-  end
 end
