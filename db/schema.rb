63c9b858ab0b9c2b0c766ea32a0fee81796370d0
yusuke-sudou <143806854+yusuke-sudou@users.noreply.github.com>
2023-11-08 05:22:33 +0000

    [Add]ジャンル機能全体

--- db/schema.rb
+++ db/schema.rb
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 2023_11_04_052113) do
   end
 
   create_table "genres", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
