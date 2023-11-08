654ba6fd2d2676b365a19617f0d9d99f5dc6a2be
yusuke-sudou <143806854+yusuke-sudou@users.noreply.github.com>
2023-11-08 07:57:50 +0000

    [Add]新規投稿・詳細・一覧機能実装

--- app/controllers/admin/menus_controller.rb
+++ app/controllers/admin/menus_controller.rb
@@ -1,14 +1,28 @@
 class Admin::MenusController < ApplicationController
   def index
+    @menus_by_genre = Menu.all.group_by(&:genre_id)
   end
 
   def new
+    @menu = Menu.new
+    @genres = Genre.all
   end
 
   def create
+    @menu = Menu.new(menu_params)
+    # ジャンルの情報を関連付けるために、ジャンルIDを設定する例
+    @menu.genre_id = params[:menu][:genre_id]
+    if @menu.save
+      # メニューの新規登録が成功した場合の処理
+      redirect_to admin_menu_path(@menu.id)
+    else
+      # メニューの新規登録が失敗した場合の処理
+      render 'new'
+    end
   end
 
   def show
+    @menu = Menu.find(params[:id])
   end
 
   def edit
@@ -16,4 +30,11 @@ class Admin::MenusController < ApplicationController
 
   def update
   end
+  
+  private
+
+  def menu_params
+    params.require(:menu).permit(:name, :price, :explannation,:genre_id)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_11_04_052113) do
+ActiveRecord::Schema.define(version: 2023_11_08_075153) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 2023_11_04_052113) do
     t.integer "price", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "genre_id"
   end
 
   create_table "post_board_comments", force: :cascade do |t|
