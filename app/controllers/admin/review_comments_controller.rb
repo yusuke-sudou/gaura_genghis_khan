01d87de014e5acc16f5e8f20eb1f6d6db2937500
yusuke-sudou <143806854+yusuke-sudou@users.noreply.github.com>
2023-11-11 06:39:10 +0000

    [Add]コメント削除機能実装

--- app/controllers/admin/review_comments_controller.rb
+++ app/controllers/admin/review_comments_controller.rb
@@ -1,2 +1,14 @@
 class Admin::ReviewCommentsController < ApplicationController
+
+  def index
+    @review_comments = ReviewComment.all
+    @users = User.all
+  end
+
+  def destroy
+    @review_comment = ReviewComment.find(params[:id])
+    @review_comment.destroy
+    redirect_to admin_review_comments_path
+  end
+
 end
