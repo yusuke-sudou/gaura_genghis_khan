d11655d0e40c1a6ef72f9ac855a57b624f12d79b
yusuke-sudou <143806854+yusuke-sudou@users.noreply.github.com>
2023-11-13 01:32:41 +0000

    [Add]レビュー新着一部調整

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,7 @@
 class Public::HomesController < ApplicationController
   def top
     # 新規作成順に並び替えて、5件のItemモデルを取得
-    @new_reviews = Review.order(created_at: :desc).limit(4)
+    @new_reviews = Review.order(created_at: :desc).limit(3)
     @users = User.all
   end
   
