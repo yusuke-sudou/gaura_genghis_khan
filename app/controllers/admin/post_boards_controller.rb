b9984c474f7da874b8a2fb5686d7df0cb1ca8452
yusuke-sudou <143806854+yusuke-sudou@users.noreply.github.com>
2023-11-16 07:49:43 +0000

    [Add]管理者側より削除機能完成

--- app/controllers/admin/post_boards_controller.rb
+++ app/controllers/admin/post_boards_controller.rb
@@ -4,4 +4,13 @@ class Admin::PostBoardsController < ApplicationController
     @post_boards = @community.post_boards
     @users = User.all
   end
+  
+  def destroy
+    @post_board = PostBoard.find(params[:id])
+    @community = @post_board.community
+    @post_board.destroy
+    flash[:notice] = "削除に成功しました。"
+    redirect_to admin_community_post_boards_path(community_id: @community.id)
+  end
+  
 end
