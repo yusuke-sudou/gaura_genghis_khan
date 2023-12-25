5914c97a7b32436a96f3025370ef04588cb4583f
yusuke-sudou <sudoukky22@gmail.com>
2023-12-25 16:42:07 +0900

    [Add]管理者側コミュニティアクセス制限

--- app/controllers/admin/communities_controller.rb
+++ app/controllers/admin/communities_controller.rb
@@ -1,5 +1,5 @@
 class Admin::CommunitiesController < ApplicationController
-  before_action :authenticate_admin!, only: [:edit, :update]
+  before_action :authenticate_admin!, only: [:index, :create, :show, :edit, :update, :destroy]
   def index
     @community = Community.new
     @communities = Community.all
