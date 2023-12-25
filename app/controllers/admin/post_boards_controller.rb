e674a56508c5ab61b58e653254b16f16e123482c
yusuke-sudou <sudoukky22@gmail.com>
2023-12-25 16:32:54 +0900

    [Add]管理者側掲示板一覧アクセス制限

--- app/controllers/admin/post_boards_controller.rb
+++ app/controllers/admin/post_boards_controller.rb
@@ -1,5 +1,6 @@
 class Admin::PostBoardsController < ApplicationController
   before_action :authenticate_admin!, only: [:index, :destroy]
+
   def index
     @community = Community.find(params[:community_id])
     @post_boards = @community.post_boards
